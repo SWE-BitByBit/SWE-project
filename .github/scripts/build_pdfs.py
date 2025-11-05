ab2fda86a8816e7f68a2f85c52b4423e9f09171f
Rmanisi <riccardomansi@gmail.com>
2025-11-05 12:54:36 +0100

    Test2 per action

--- .github/scripts/build_pdfs.py
+++ .github/scripts/build_pdfs.py
@@ -10,7 +10,7 @@ IGNORED_FILES = {"spazio_firma.tex"}
 def main():
     tex_files = [f for f in SRC_DIR.rglob("*.tex") if f.name not in IGNORED_FILES]
     if not tex_files:
-        print("⚠️ Nessun file .tex da compilare trovato in src/")
+        print("Nessun file .tex da compilare trovato in src/")
         return
 
     for tex_file in tex_files:
@@ -22,15 +22,15 @@ def main():
 
         try:
             subprocess.run(
-                [
-                    "bash",
-                    "-c",
-                    f"cd '{tex_file.parent}' && "
-                    f"latexmk -pdf -interaction=nonstopmode -halt-on-error "
-                    f"-outdir='{output_dir.resolve()}' '{tex_file.name}'"
-                ],
-                check=True
-            )
+                            [
+                                "bash",
+                                "-c",
+                                f"cd '{tex_file.parent}' && "
+                                f"latexmk -pdf -interaction=nonstopmode -halt-on-error -quiet "
+                                f"-outdir='{output_dir.resolve()}' '{tex_file.name}'"
+                            ],
+                            check=True
+                        )
 
         except subprocess.CalledProcessError as e:
             print(f"❌ Errore durante la compilazione di {tex_file}")
