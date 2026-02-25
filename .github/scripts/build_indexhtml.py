8e9b9362d47a60e056ad2def21ffe78e4d325629
ffracasso-B3 <ferdinando.fracasso@studenti.unipd.it>
2026-02-25 18:43:18 +0100

    script

--- .github/scripts/build_indexhtml.py
+++ .github/scripts/build_indexhtml.py
@@ -20,7 +20,7 @@ def create_sec_links(file):
     else:
         #è verbale
         filename = ''
-        if file.full_match('/verbali_interni/**.pdf'):  #verbale interno
+        if fn.fnmatch(file.as_posix(),'*[/verbali_interni/]*.pdf'):  #verbale interno
             filename = file.name.replace('interno_','').replace('verbale_', 'Verbale interno del ').replace('_',' ').replace('-','/')
         else: #verbale esterno
             filename = file.name.replace('esterno_','').replace('verbale_', 'Verbale esterno del ').replace('_',' ').replace('-','/')
