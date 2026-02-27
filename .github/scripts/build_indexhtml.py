d21f9ac7a11a1bcc7cc3839419ecfd58d0485d3f
ffracasso-B3 <ferdinando.fracasso@studenti.unipd.it>
2026-02-27 13:44:38 +0100

    Revert tentativo sort

--- .github/scripts/build_indexhtml.py
+++ .github/scripts/build_indexhtml.py
@@ -49,8 +49,8 @@ with open(Path('site_template.txt'), 'r') as temp:
 doc_candidatura = list(C_PATH_DOCS.rglob("*.pdf"))
 #rimuovi verbali da doc candidatura
 doc_candidatura = [f for f in doc_candidatura if f.exists and not(fn.fnmatch(f.name,"*verbale*"))]
-verb_esterni_candidatura = list(C_PATH_VERB_EXT.rglob("*.pdf")).sort()
-verb_interni_candidatura = list(C_PATH_VERB_INT.rglob("*.pdf")).sort()
+verb_esterni_candidatura = list(C_PATH_VERB_EXT.rglob("*.pdf"))
+verb_interni_candidatura = list(C_PATH_VERB_INT.rglob("*.pdf"))
 
 esterni_rtb = list(RTB_PATH_ESTERNI.rglob("*.pdf"))
 interni_rtb = list(RTB_PATH_INTERNI.rglob("*.pdf"))
