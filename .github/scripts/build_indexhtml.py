da96902ab228b83cb82d304cdf5c981d10cfe310
ffracasso-B3 <ferdinando.fracasso@studenti.unipd.it>
2026-03-31 16:24:56 +0200

    Tentativo fix errore 4

--- .github/scripts/build_indexhtml.py
+++ .github/scripts/build_indexhtml.py
@@ -32,9 +32,7 @@ def create_doc_link(file):
 def get_file_vers(file) -> str:
     #Apri file tex src e recupera le informazioni di versione
     srcfilepath = file.parents[0].as_posix().replace('docs','src')
-    print(f"{srcfilepath}")
-    srcfile = list(Path(srcfilepath).rglob("*.tex"))
-    print(len(srcfile))
+    srcfile = list(Path(srcfilepath).rglob("*.tex"))[0]
     versione = ''
     with open(srcfile, "r", encoding="utf-8", errors="ignore") as f:
         section = re.search(r"\\textbf{Versione:}\s*&\s([\d\.]+)\s*\\\\", f)
