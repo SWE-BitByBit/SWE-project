5af243b3fb55179141f7170e3d3be6a6b022a245
ffracasso-B3 <ferdinando.fracasso@studenti.unipd.it>
2026-03-31 16:01:24 +0200

    tentativo fix errore 1

--- .github/scripts/build_indexhtml.py
+++ .github/scripts/build_indexhtml.py
@@ -31,7 +31,8 @@ def create_doc_link(file):
 
 def get_file_vers(file) -> str:
     #Apri file tex src e recupera le informazioni di versione
-    srcfile = list(Path(file.parent().resolve().replace('docs','src')).rglob("*.tex"))[0]
+    srcfilepath = file.parent().resolve().replace('docs','src')
+    srcfile = list(Path(srcfilepath).rglob("*.tex"))[0]
     versione = ''
     with open(srcfile, "r", encoding="utf-8", errors="ignore") as f:
         section = re.search(r"\\textbf{Versione:}\s*&\s([\d\.]+)\s*\\\\", f)
